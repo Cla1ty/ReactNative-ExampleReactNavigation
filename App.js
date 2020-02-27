9c3912517d9aa29fce72591e6df8604a930871ab
Dwi Ariyanto <dwi.ariyanto@jakmall.com>
2020-02-27 16:01:36 +0700

    feat: navigate params passing previous screen

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, TextInput} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-function HomeScreen({navigation}) {
+function HomeScreen({navigation, route}) {
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params]);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
@@ -16,7 +23,35 @@ function HomeScreen({navigation}) {
           });
         }}
       />
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{margin: 10}}>Post: {route.params?.post}</Text>
     </View>
+  );
+}
+
+function CreatePostScreen({navigation, route}) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{height: 200, padding: 10, backgroundColor: 'white'}}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass params back to home screen
+          navigation.navigate('Home', {post: postText});
+        }}
+      />
+    </>
   );
 }
 
@@ -57,6 +92,7 @@ function App() {
           component={HomeScreen}
           options={{title: 'Overview'}}
         />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
