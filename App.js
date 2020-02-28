5bb502f964890ff43ba4cd5dd671c710c2248eaa
Dwi Ariyanto <dwi.ariyanto@jakmall.com>
2020-02-28 14:42:21 +0700

    navigation badges icon

--- App.js
+++ App.js
@@ -123,6 +123,33 @@ function ModalScreen({navigation}) {
   );
 }
 
+function IconWithBadge({name, badgeCount, color, size}) {
+  return (
+    <View style={{width: 24, height: 24, margin: 5}}>
+      <Icon name={name} size={size} color={color} />
+      {badgeCount > 0 && (
+        <View
+          style={{
+            // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+            position: 'absolute',
+            right: -6,
+            top: -3,
+            backgroundColor: 'red',
+            borderRadius: 6,
+            width: 12,
+            height: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{color: 'white', fontSize: 10, fontWeight: 'bold'}}>
+            {badgeCount}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
 const HomeStack = createStackNavigator();
 
 function HomeStackScreen() {
@@ -183,7 +210,14 @@ function MainTabScreen() {
           }
 
           // You can return any component that you like here!
-          return <Icon name={iconName} size={size} color={color} />;
+          return (
+            <IconWithBadge
+              name={iconName}
+              badgeCount={3}
+              size={size}
+              color={color}
+            />
+          );
         },
       })}
       tabBarOptions={{
